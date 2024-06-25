a17869e6ac2532d29e1578f1fb34c1ff20d3ea21
Michuxx <104158118+Michuxx@users.noreply.github.com>
2024-06-25 16:34:58 +0200

    Refs added

--- ProjectManagement/src/components/NewProject.jsx
+++ ProjectManagement/src/components/NewProject.jsx
@@ -6,6 +6,12 @@ export default function NewProject({ OnCancelProject }) {
   const dueDateRef = useRef();
   const descriptionRef = useRef();
 
+  const handleSave = () => {
+    const enteredTitle = titleRef.current.value;
+    const enteredDueDateRef = dueDateRef.current.value;
+    const enteredDescriptionRef = descriptionRef.current.value;
+  };
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
@@ -18,7 +24,10 @@ export default function NewProject({ OnCancelProject }) {
           </button>
         </li>
         <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+          <button
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            onClick={handleSave}
+          >
             Save
           </button>
         </li>
